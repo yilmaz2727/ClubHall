6516253ddc47113f53ce5d7cb1228a1fa83ca0eb
Huseyin Yilmaz <hy97556@gmail.com>
2025-10-24 14:39:38 +0300

    İlk commit: öğrenci kulüp sistemi eklendi

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace OgrenciKulupSistemi.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
