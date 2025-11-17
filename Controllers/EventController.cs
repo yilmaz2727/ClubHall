42ba3c15790168bd354187977964077b228ec679
Ahmet Oğan <ahmetoganasd@gmail.com>
2025-11-17 18:49:50 +0300

    feat: Implement EventController to get events from database

--- Controllers/EventController.cs
+++ Controllers/EventController.cs
@@ -1,14 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OgrenciKulupSistemi.Data;
 
 namespace OgrenciKulupSistemi.Controllers
 {
 
     public class EventController : Controller
     {
-
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+    
+        public EventController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var events = await _context.Events.ToListAsync();
+            return View(events);
         }
 
     }
