49450f82ce70b53b5776880cd24d9c99a9984391
Huseyin Yilmaz <hy97556@gmail.com>
2025-11-20 23:02:25 +0300

    membership ve event sayfaları yaratıldı profilde profil sayfası  düzenlendi

--- Controllers/ProfileController.cs
+++ Controllers/ProfileController.cs
@@ -1,12 +1,32 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using OgrenciKulupSistemi.Models;
 
 namespace OgrenciKulupSistemi.Controllers
 {
 
     public class ProfileController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public IActionResult Index()
+    public ProfileController(UserManager<ApplicationUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+      public async Task<IActionResult> Index()
+     {
+    var user = await _userManager.GetUserAsync(User);
+    return View(user);   
+      }
+
+
+        public IActionResult MyEvents()
+        {
+            return View();
+        }
+        
+        public IActionResult MyMemberShips()
         {
             return View();
         }
