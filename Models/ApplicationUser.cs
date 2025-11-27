d3a868d47bf97063ddee82864d5807bc541ef7d0
Huseyin Yilmaz <hy97556@gmail.com>
2025-11-27 12:20:30 +0300

    ApplicationUser daki proportylere sorı işareti eklendi

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -11,12 +11,12 @@ namespace OgrenciKulupSistemi.Models
 
     public class ApplicationUser : IdentityUser
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string BirthPlace { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? BirthPlace { get; set; }
         public DateTime? BirthDate { get; set; }
-        public string Gender { get; set; }
-          public string City { get; set; }
+        public string? Gender { get; set; }
+          public string? City { get; set; }
     }
 
 }
