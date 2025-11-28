c993bcfd18414fcb54e43f572cf7dc5075993bc5
ahilmii <ahmet1905hilmi@gmail.com>
2025-11-28 21:53:11 +0300

    gecis modeli eklendi

---
+++ Models/UserEditViewModel.cs
@@ -0,0 +1,12 @@
+namespace OgrenciKulupSistemi.Models
+{
+    public class UserEditViewModel
+    {
+        public string? City { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Email { get; set; }
+        public string? BirthPlace { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public string? Gender { get; set; }
+    }
+}
