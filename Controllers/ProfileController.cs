97f3d0bc99e5b5e8649b5330b60363f6272bbe8a
ahilmii <ahmet1905hilmi@gmail.com>
2025-11-29 01:03:04 +0300

    comment lines added

--- Controllers/ProfileController.cs
+++ Controllers/ProfileController.cs
@@ -25,14 +25,29 @@ namespace OgrenciKulupSistemi.Controllers
             return View(user);
         }
 
+        /* 
+        Önemli Not: ApplciationUser.cs sınıfı veritabanındaki tabloyu temsil eder. UserEditViewModel ise geçici bir taşıyıcı.
+        Bu sınıf sadece kullanıcıya ekranda form gösterirken ve kullanıcıdan veriyi alırken kullanılır.
+        
+        Kullanıcı "Düzenle"ye Tıklar (GET):
+            DB'den ApplicationUser'ı çekersin.
+            İçindeki verileri UserEditViewModel'e kopyalarsın.
+            Kullanıcıya UserEditViewModel ile dolu HTML sayfasını gösterirsin (Kullanıcı DB nesnesini hiç görmez).
+
+        Kullanıcı Bilgileri Değiştirip "Kaydet" Der (POST):
+            Veriler UserEditViewModel olarak Controller'a geri gelir.
+            Sen tekrar DB'den ApplicationUser'ı (orijinal kaydı) çağırırsın.
+            Bu sefer tersine kopyalama yaparsın: ViewModel'deki yeni verileri -> ApplicationUser üzerine yazarsın.
+        */
+
 
         [HttpGet]
         public async Task<IActionResult> EditPersonalInfo()
         {
-            var user = await _userManager.GetUserAsync(User);
+            var user = await _userManager.GetUserAsync(User); // o an siteye giriş yapmış olan kullanıcını tüm bilgilerini alır (ApplicationUser nesnesi)
 
-            // Veritabanından (ApplicationUser) -> Ekrana (UserEditViewModel) dönüştürüyoruz
-            var model = new UserEditViewModel
+            // user değişkenni PAssword gibi gizli bilgiler dahil birçok alanı içerir. bunu doğrudan kulanamayız. sadce içerisinde bulunan City,, BirthPlace gibi bilgileri kullanmalıyız burada, gizlilik açısından
+            var model = new UserEditViewModel // --> Veritabanından (ApplicationUser) -> Ekrana (UserEditViewModel) dönüştürüyoruz
             {
                 City = user.City,
                 PhoneNumber = user.PhoneNumber,
